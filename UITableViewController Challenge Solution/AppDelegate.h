4b31d7ba3e5d790fd484a8b738183b463c54de3e
jagjassam <jagjassam@gmail.com>
2015-02-07 11:15:18 -0800

    Initial Commit

---
+++ UITableViewController Challenge Solution/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UITableViewController Challenge Solution
+//
+//  Created by Jagmeet on 2/7/15.
+//  Copyright (c) 2015 Think Ninja Cut. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UITableViewController Challenge Solution/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UITableViewController Challenge Solution
+//
+//  Created by Jagmeet on 2/7/15.
+//  Copyright (c) 2015 Think Ninja Cut. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
